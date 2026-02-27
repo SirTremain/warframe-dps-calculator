5168c3a0e78a1bb26402c9c8caa6fc0ec2c40b0c
Markopteryx <marko.uksanovic1@gmail.com>
2026-02-27 20:31:24 +1000

    styling: remove all .css and use only tailwind

--- packages/web/src/components/hero.rs
+++ packages/web/src/components/hero.rs
@@ -5,20 +5,16 @@ const HEADER_SVG: Asset = asset!("/assets/header.svg");
 #[component]
 pub fn Hero() -> Element {
     rsx! {
-        // We can create elements inside the rsx macro with the element name followed by a block of attributes and children.
         div {
-            // Attributes should be defined in the element before any children
-            id: "hero",
-            // After all attributes are defined, we can define child elements and components
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                // The RSX macro also supports text nodes surrounded by quotes
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            class: "flex flex-col justify-center items-center",
+            img { src: HEADER_SVG, class: "max-w-[1200px]" }
+            div { class: "w-[400px] text-left text-xl text-white flex flex-col",
+                a { href: "https://dioxuslabs.com/learn/0.7/", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", class: "text-white no-underline my-2.5 border border-white rounded-[5px] p-2.5 hover:bg-[#1f1f1f] hover:cursor-pointer", "👋 Community Discord" }
             }
         }
     }
